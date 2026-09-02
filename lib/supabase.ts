d9d53aafee0f4cd8d3557a6a34d57c1b6330f831
ntta5743-beep <ntta5743@gmail.com>
2026-09-02 16:52:40 +0200

    Delete lib directory

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://puqvjdrqefzhqavhryso.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_WHA1Rx2KQStqfIfy0j5vtw__0ra0pl_'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
